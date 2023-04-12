42cc2cc0f17e5c56edde2ac9871c38f9199f1d30
M. Grooms jr <melvingrooms@gmail.com>
2023-04-12 01:13:47 +0000

    create-dynamodb-table

---
+++ create-dynamodb.py
@@ -0,0 +1,24 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.create_table(
+   TableName='movies', 
+   KeySchema=[
+        {
+            'AttributeName': 'id',
+            'KeyType': 'HASH'
+        },
+    ], 
+      AttributeDefinitions=[
+        {
+            'AttributeName': 'id',
+            'AttributeType': 'N'
+        },
+    ],
+    ProvisionedThroughput={
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+    }        
+)
+print("Table status", table.table_status)
